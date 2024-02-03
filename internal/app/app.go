e133f59e4f06b66ae1db7210f220ff3737bb1ccd
alextotalk <alexoo@inbox.ru>
2024-02-04 01:11:57 +0200

    Initial commit

---
+++ internal/app/app.go
@@ -0,0 +1,48 @@
+// nolint: funlen
+package app
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// Run initializes whole application.
+func Run() {
+
+	// HTTP Server
+	srv := server.NewServer(cfg, handlers.Init(cfg))
+
+	go func() {
+		if err := srv.Run(); !errors.Is(err, http.ErrServerClosed) {
+			logger.Errorf("error occurred while running http server: %s\n", err.Error())
+		}
+	}()
+
+	logger.Info("Server started")
+
+	// Graceful Shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	const timeout = 5 * time.Second
+
+	ctx, shutdown := context.WithTimeout(context.Background(), timeout)
+	defer shutdown()
+
+	if err := srv.Stop(ctx); err != nil {
+		l ogger.Errorf("failed to stop server: %v", err)
+	}
+
+	if err := mongoClient.Disconnect(context.Background()); err != nil {
+		logger.Error(err.Error())
+	}
+}
+
+
